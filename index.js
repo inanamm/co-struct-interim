afa8e41f3ecc7dee2ba29801d2f5f2b19b456698
inanamm <martina.holenstein@gmail.com>
2023-02-25 22:30:14 +0100

    anim

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const backgroundImages = ['url(../assets/Image_1.jpg)', 'url(../assets/Image_2.jpg)', 'url(../assets/Image_3.jpg)']
 
-const backgroundImage = backgroundImages[Math.floor(Math.random()*backgroundImages.length)];
+const backgroundImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
 
 let style = document.querySelector('body').style;
 style.setProperty('--background', backgroundImage);
 
+const timeLine = anime.timeline()
+    .add({
+        targets: '.logo',
+        opacity: '100%',
+        duration: 800,
+        easing: 'easeInOutCubic'
+    })
+    .add({
+        targets: '.dash',
+        width: '100%',
+        duration: 3000,
+        easing: 'easeInOutCubic'
+    }, '-=100')
+    .add({
+        targets: 'footer',
+        opacity: '100%',
+        duration: 400,
+        easing: 'easeInOutCubic'
+    }, '-=800')
 
 
-anime({
-    targets: '.dash',
-    width: '100%',
-    duration: 8000,
-    easing: 'easeInOutCubic'
-  });
+
